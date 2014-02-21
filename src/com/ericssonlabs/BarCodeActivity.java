a1dda116aa150c1a4f1f8ecf13dbd4fe932daddc
renjie120 <lishuiqing110@163.com>
2014-02-21 11:43:20 +0800

    优化二维码扫描

--- src/com/ericssonlabs/BarCodeActivity.java
+++ src/com/ericssonlabs/BarCodeActivity.java
@@ -1,76 +1,100 @@
 package com.ericssonlabs;
 
-import com.google.zxing.WriterException;
-import com.zxing.activity.CaptureActivity;
-import com.zxing.encoding.EncodingHandler;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.ericssonlabs.bean.EventInfo;
+import com.ericssonlabs.bean.ServerResult;
+import com.ericssonlabs.util.Constant;
+import com.zxing.activity.CaptureActivity;
 
 /**
- * 二维码主界面.
+ * 二维码签到.
  */
 public class BarCodeActivity extends Activity {
-    /** Called when the activity is first created. */
 	private TextView resultTextView;
-	private EditText qrStrEditText;
-	private ImageView qrImgImageView;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.barcode);
-        
-        resultTextView = (TextView) this.findViewById(R.id.tv_scan_result);
-        qrStrEditText = (EditText) this.findViewById(R.id.et_qr_string);
-        qrImgImageView = (ImageView) this.findViewById(R.id.iv_qr_image);
-        
-        Button scanBarCodeButton = (Button) this.findViewById(R.id.btn_scan_barcode);
-        scanBarCodeButton.setOnClickListener(new OnClickListener() {
-			
+	private String token;
+	private String eventid;
+
+	private EventInfo qiandao(String tickid) {
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+		String encoding = "UTF-8";
+		try {
+
+			HttpPost httpost = new HttpPost(Constant.HOST
+					+ "?do=checkticket&ticketid="+tickid+"&check=true&token="
+					+ token);
+			HttpResponse response = httpclient.execute(httpost);
+			HttpEntity entity = response.getEntity();
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					entity.getContent(), encoding));
+			String sss = br.readLine();
+			// 如果没有登录成功，就弹出提示信息.
+			ServerResult result = (ServerResult) JSON.parseObject(sss,
+					ServerResult.class);
+
+			EventInfo t = (EventInfo) JSON.parseObject(result.getData()
+					.toJSONString(), EventInfo.class);
+			Message msg = new Message();
+			msg.what = 2;
+			Bundle b = new Bundle();
+			b.putString("starttime", t.getStarttime());
+			b.putString("endtime", t.getEndtime());
+			b.putString("name", t.getName());
+			b.putString("url", t.getImageurl());
+			msg.setData(b); 
+			return t;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			httpclient.getConnectionManager().shutdown();
+		}
+		return null;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.barcode);
+
+		resultTextView = (TextView) this.findViewById(R.id.result);
+		Intent intent = getIntent();
+		token = intent.getStringExtra("token");
+		eventid = intent.getStringExtra("eventid");
+		Button scanBarCodeButton = (Button) this
+				.findViewById(R.id.btn_scan_barcode);
+		scanBarCodeButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				//打开扫描界面扫描条形码或二维码
-				Intent openCameraIntent = new Intent(BarCodeActivity.this,CaptureActivity.class);
+				// 打开扫描界面扫描条形码或二维码
+				Intent openCameraIntent = new Intent(BarCodeActivity.this,
+						CaptureActivity.class);
 				startActivityForResult(openCameraIntent, 0);
 			}
 		});
-        
-        Button generateQRCodeButton = (Button) this.findViewById(R.id.btn_add_qrcode);
-        generateQRCodeButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				try {
-					String contentString = qrStrEditText.getText().toString();
-					if (!contentString.equals("")) {
-						//根据字符串生成二维码图片并显示在界面上，第二个参数为图片的大小（350*350）
-						Bitmap qrCodeBitmap = EncodingHandler.createQRCode(contentString, 350);
-						qrImgImageView.setImageBitmap(qrCodeBitmap);
-					}else {
-						Toast.makeText(BarCodeActivity.this, "Text can not be empty", Toast.LENGTH_SHORT).show();
-					}
-					
-				} catch (WriterException e) { 
-					e.printStackTrace();
-				}
-			}
-		});
-    }
+
+	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		//处理扫描结果（在界面上显示）
+		// 处理扫描结果（在界面上显示）
 		if (resultCode == RESULT_OK) {
 			Bundle bundle = data.getExtras();
 			String scanResult = bundle.getString("result");
